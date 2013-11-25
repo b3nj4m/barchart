e2d68d5438186f1995e6af3525fd8912c02fb800
Ben Robbins <ben.robbins@gmail.com>
2013-11-24 18:00:20 -0600

    add multi-dataset support, remove old examples folder Squashed commit of the following:
    commit 2c53f2bcaba674ad2f43cc161fceedfe094dae11
    Author: Ben Robbins <ben.robbins@gmail.com>
    Date:   Sun Nov 24 17:56:31 2013 -0600
    
        tolerate 1d array
    
    commit beaaa695d83b45b9b29fdcc22e1f3b75b52b2b43
    Author: Ben Robbins <ben.robbins@gmail.com>
    Date:   Sun Nov 24 17:52:54 2013 -0600
    
        cleanup
    
    commit 1c203b3f03f89f14d16e125fc810592e93f6e8c1
    Author: Ben Robbins <ben.robbins@gmail.com>
    Date:   Sun Nov 24 17:27:40 2013 -0600
    
        working multi-dataset support
    
    commit 684d52e1ee0f115b8b40e49b0d86faba40254c6b
    Author: Ben Robbins <ben.robbins@gmail.com>
    Date:   Fri Nov 22 12:15:05 2013 -0600
    
        flatten datasets
    
    commit ab86cabfe164676f322b904a8787d1a27b2dc0a0
    Author: Ben Robbins <ben.robbins@gmail.com>
    Date:   Fri Nov 22 11:26:20 2013 -0600
    
        cleanup, pad datasets to same length
    
    commit 0d1fceac6b5493184756b25b90d394fc56a23805
    Author: Ben Robbins <ben.robbins@bazaarvoice.com>
    Date:   Wed May 1 13:31:53 2013 -0500
    
        started adding multi-dataset support
    
    commit 42a90bc2aa97f8ab9556742c2ce9b62e6433f929
    Author: Ben Robbins <ben.robbins@bazaarvoice.com>
    Date:   Tue Mar 26 10:47:29 2013 -0500
    
        removing old examples folder
    
    commit 22557871aa0bfc85c9dce9fdaf89adc80570ac97
    Author: Ben Robbins <ben.robbins@bazaarvoice.com>
    Date:   Wed Mar 20 15:59:36 2013 -0500
    
        more notes
    
    commit 1c99a0edb5794a7f2c9016e51e96ba1eddb04107
    Author: Ben Robbins <ben.robbins@bazaarvoice.com>
    Date:   Wed Mar 20 15:21:54 2013 -0500
    
        adding notes about multiple datasets

--- barchart.js
+++ barchart.js
@@ -1,5 +1,30 @@
+//TODO should have pretty set of default colors for up to X datasets
+//TODO hover states and click events
+//TODO allow user to artificially override the extrema
+//TODO allow as much as possible to be overriden with css
+//TODO graceful failure when data is missing values or labels, etc.
 //TODO tooltip showing full value
+//TODO best way to implement tooltip without coupling to a particular library?
+
 (function() {
+  var isArray = function(obj) {
+    return window.Object.prototype.toString.call(obj) === '[object Array]';
+  };
+
+  var log = function(message) {
+    if (window.console && window.console.log) {
+      window.console.log(message);
+    }
+  };
+
+  var uniqueId = (function() {
+    var current = 0;
+
+    return function() {
+      return current++;
+    };
+  }());
+
   var defineBarChart = function(d3) {
     var Chart = function(options) {
       if (options) {
@@ -8,66 +33,104 @@
             this[i] = options[i];
         }
       }
-  
+
+      if (d3.scale[this.height_scale_type] === undefined) {
+        log('invalid height_scale_type "' + this.height_scale_type + '", using "' + Chart.prototype.height_scale_type + '" instead');
+        this.height_scale_type = Chart.prototype.height_scale_type;
+      }
+
+      if (this.data === undefined || !isArray(this.data)) {
+        log('invalid data. data should be an array');
+        this.data = [this.data];
+      }
+
+      if (!isArray(this.data[0])) {
+        this.data = [this.data];
+      }
+
       this.make_accessors();
+
+      if (this.data.length > 0) {
+        if (isArray(this.data[0])) {
+          this.num_datasets = this.data.length;
+        }
+        else {
+          this.num_datasets = 1;
+          this.data = [this.data];
+        }
+        this.dataset_size = d3.max(this.data, function(arr) { return arr.length; });
+        this.num_records = this.num_datasets * this.dataset_size;
+
+        // transform array of datasets into flat array with ordering like:
+        // [dataset1_record1, dataset2_record1, ..., dataset1_record2, dataset2_record2, ...]
+        var data = new Array(this.num_records);
+        var idx;
+        for (var record_num = 0; record_num < this.dataset_size; record_num++) {
+          for (var dataset_num = 0; dataset_num < this.num_datasets; dataset_num++) {
+            idx = record_num * this.num_datasets + dataset_num;
+            data[idx] = this.data[dataset_num][record_num];
+            data[idx].__id = uniqueId();
+          }
+        }
+
+        this.data = data;
+
+        var extrema = d3.extent(this.data, this.data_value_accessor);
+        this.minimum = extrema[0];
+        this.maximum = extrema[1];
+      }
     };
   
     Chart.prototype.make_accessors = function() {
       var chart = this;
-      this.data_value_accessor = function(d) {
-        if (d !== undefined && d !== null)
+      chart.data_value_accessor = function(d) {
+        if (typeof d !== 'undefined' && d !== null) {
           return d[chart.data_value_key];
+        }
       };
-      this.label_top_accessor = function(d) {
-        if (d !== undefined && d !== null)
+      chart.label_top_accessor = function(d) {
+        if (typeof d !== 'undefined' && d !== null) {
           return d[chart.label_top_key];
+        }
       };
-      this.label_inside_accessor = function(d) {
-        if (d !== undefined && d !== null)
+      chart.label_inside_accessor = function(d) {
+        if (typeof d !== 'undefined' && d !== null) {
           return d[chart.label_inside_key];
+        }
       };
-      this.data_id_key_accessor = function(d) {
-        if (d !== undefined && d !== null)
-          return d[chart.data_id_key];
-      };
-      this.x_scale_accessor = function(d, i) {
+      chart.x_scale_accessor = function(d, i) {
         return chart.x_scale(i);
       };
-      this.x_scale_px_accessor = function(d, i) {
+      chart.x_scale_px_accessor = function(d, i) {
         return chart.x_scale_accessor(d, i) + 'px';
       };
-      this.y_scale_accessor = function(d, i) {
+      chart.y_scale_accessor = function(d, i) {
         return chart.y_scale(chart.data_value_accessor(d));
       };
-      this.y_scale_px_accessor = function(d, i) {
+      chart.y_scale_px_accessor = function(d, i) {
         return chart.y_scale_accessor(d, i) + 'px';
       };
-      this.label_top_y_scale_accessor = function(d, i) {
+      chart.label_top_y_scale_accessor = function(d, i) {
         return chart.label_top_y_scale(chart.data_value_accessor(d), this);
       };
-      this.label_top_y_scale_px_accessor = function(d, i) {
+      chart.label_top_y_scale_px_accessor = function(d, i) {
         return chart.label_top_y_scale_accessor.apply(this, [d, i]) + 'px';
       };
-      this.height_scale_accessor = function(d, i) {
+      chart.height_scale_accessor = function(d, i) {
         return chart.height_scale(chart.data_value_accessor(d));
       };
-      this.height_scale_px_accessor = function(d, i) {
+      chart.height_scale_px_accessor = function(d, i) {
         return chart.height_scale_accessor(d, i) + 'px';
+      };
+      chart.bar_color_accessor = function(d, i) {
+        return chart.bar_colors[i % chart.num_datasets] || '#CCC';
       };
     };
   
-    Chart.prototype.data_value_accessor = function(d) {
-      return d.value;
+    Chart.prototype.data_id_accessor = function(d, i) {
+      return d.__id;
     };
-  
-    Chart.prototype.data_label_top_accessor = function(d) {
-      return d.label_top;
-    };
-  
-    Chart.prototype.data_label_inside_accessor = function(d) {
-      return d.label_inside;
-    };
-  
+ 
     Chart.prototype.animation_delay = function(d, i) {
       return i * 100;
     };
@@ -77,12 +140,14 @@
     Chart.prototype.is_animated = true;
     Chart.prototype.animation_duration = 400;
     Chart.prototype.auto_scale = false;
-    Chart.prototype.bar_color = '#00AB8E';
-    Chart.prototype.bar_spacing = 20;
-    Chart.prototype.chart_padding = 10;
+    Chart.prototype.height_scale_type = 'linear';
+    Chart.prototype.bar_colors = ['#00AB8E', '#33CCDD'];
+    Chart.prototype.bar_spacing = 2;
+    Chart.prototype.group_spacing = 8;
+    Chart.prototype.chart_padding = 0;
     Chart.prototype.data = null;
+    Chart.prototype.numDatasets = 0;
     Chart.prototype.data_value_key = 'value';
-    Chart.prototype.data_id_key = 'id';
     Chart.prototype.height = 300;
     Chart.prototype.width = 700;
     Chart.prototype.label_top_color = '#003D4C';
@@ -97,18 +162,29 @@
         this.width = this.container_elem.scrollWidth || this.width;
       }
   
+      if (this.data.length === 0) {
+        return;
+      }
+
       this.min_bar_size = this.label_size + this.label_padding * 2;
       this.max_bar_size = Math.max(this.min_bar_size, this.height - this.chart_padding - this.label_size - this.label_padding * 2);
-      this.data_min = d3.min(this.data, this.data_value_accessor);
-      this.data_max = d3.max(this.data, this.data_value_accessor);
-  
-      this.bar_width = Math.floor((this.width - this.chart_padding * 2 - (this.data.length - 1) * this.bar_spacing) / this.data.length);
+
+      this.bar_width = Math.floor((this.width - this.chart_padding * 2 - (this.num_records - 1) * this.bar_spacing - (this.dataset_size - 1) * this.group_spacing) / this.num_records);
       this.label_width = this.bar_width - this.label_padding * 2;
   
-      var height_scale = d3.scale.linear().domain([this.data_min, this.data_max]).range([this.min_bar_size, this.max_bar_size])
+      var height_scale = d3.scale[this.height_scale_type]().domain([this.minimum, this.maximum]).range([this.min_bar_size, this.max_bar_size])
       this.height_scale = height_scale;
-  
-      this.x_scale = d3.scale.linear().domain([0, this.data.length - 1]).range([this.chart_padding, this.width - this.chart_padding - this.bar_width])
+
+      //record-group scale which maps group number to x-coord of left-most bar in the group
+      var group_x_scale = d3.scale.linear().domain([0, this.dataset_size - 1]).range([this.chart_padding, this.width - this.chart_padding - this.bar_width * this.num_datasets - (this.num_datasets - 1) * this.bar_spacing]);
+      //record scale which maps idx within record-group to offset inside record group
+      var bar_x_scale = d3.scale.linear().domain([0, this.num_datasets - 1]).range([0, (this.num_datasets - 1) * (this.bar_width + this.bar_spacing)]);
+
+      this.x_scale = function(idx) {
+        var group_num = Math.floor(idx / this.num_datasets);
+        var dataset_num = idx % this.num_datasets;
+        return group_x_scale(group_num) + bar_x_scale(dataset_num);
+      };
   
       var baseline = this.height - this.chart_padding;
       var y_scale = function(val) {
@@ -120,22 +196,25 @@
       var label_size = this.label_size;
       this.label_top_y_scale = function(val, ctx) {
         var y = y_scale(val) - label_padding;
-        if (ctx !== undefined && ctx.scrollHeight)
+        if (ctx !== undefined && ctx.scrollHeight) {
           y = y - ctx.scrollHeight;
+        }
         return y;
       };
     };
   
     Chart.prototype.render = function() {
       if (!this.has_rendered) {
-        if (this.container === undefined)
+        if (this.container === undefined) {
           this.container = document.body;
+        }
   
         this.$container = d3.select(this.container).append('div').style('position', 'relative');
         this.container_elem = this.$container[0][0];
   
-        if (this.data === undefined || this.data === null || this.data.length == 0)
+        if (this.data === undefined || this.data === null || this.data.length == 0) {
           return;
+        }
   
         this.compute_boundaries();
   
@@ -152,19 +231,19 @@
           .style('width', this.width + 'px');
       }
   
-      var bars = this.svg.selectAll('rect').data(this.data, this.data_id_key_accessor);
-      var labels_top = this.$container.selectAll('.label-top').data(this.data, this.data_id_key_accessor);
-      var labels_inside = this.$container.selectAll('.label-inside').data(this.data, this.data_id_key_accessor);
+      var bars = this.svg.selectAll('rect').data(this.data, this.data_id_accessor);
+      var labels_top = this.$container.selectAll('.label-top').data(this.data, this.data_id_accessor);
+      var labels_inside = this.$container.selectAll('.label-inside').data(this.data, this.data_id_accessor);
   
       var bars_enter = bars.enter();
       var chart = this;
       if (!bars_enter.empty()) {
         bars_enter = bars_enter.append('rect')
           .attr('x', this.x_scale_accessor)
-          .attr('y', this.y_scale(this.data_min))
+          .attr('y', this.y_scale(this.minimum))
           .attr('width', this.bar_width)
-          .attr('height', this.height_scale(this.data_min))
-          .style('fill', this.bar_color);
+          .attr('height', this.height_scale(this.minimum))
+          .style('fill', this.bar_color_accessor);
   
         labels_top.enter().append('div')
           .classed('label-top', true)
@@ -172,7 +251,7 @@
           .style('position', 'absolute')
           .style('color', this.label_top_color)
           .style('top', function(d, i) {
-            return chart.label_top_y_scale(chart.data_min, this) + 'px';
+            return chart.label_top_y_scale(chart.minimum, this) + 'px';
           })
           .style('left', this.x_scale_px_accessor)
           .style('width', this.bar_width + 'px')
@@ -188,14 +267,14 @@
           .style('width', this.bar_width + 'px')
           .style('height', this.height_scale_px_accessor)
         .append('div')
-          .data(this.data, this.data_id_key_accessor)
+          .data(this.data, this.data_id_accessor)
           .style('position', 'absolute')
           .style('color', this.label_inside_color)
           .style('bottom', '0')
           .style('left', '0')
           .style('text-align', 'center')
           .style('width', '100%')
-          .text(this.data_min)
+          .text(this.minimum)
       }
       
       bars.transition()
@@ -210,14 +289,18 @@
         .style('top', this.label_top_y_scale_px_accessor);
   
       //if we have positive numbers less than 5 digits in length, animate them!
-      if (this.data_min >= 1 && this.data_max < 10000 && this.data_max - this.data_min > 10) {
+      if (this.minimum >= 1 && this.maximum < 10000 && this.maximum - this.minimum > 10) {
         labels_inside.selectAll('div')
-          .data(this.data, this.data_id_key_accessor)
-        .transition()
+          .data(this.data, this.data_id_accessor)
+          .transition()
           .tween('label_inside_text', function(d) {
-            var i = d3.interpolateRound(Math.round(chart.data_min), parseInt(chart.data_value_accessor(d)));
+            //using height_scale to ensure that the values shown are consistent with the exact scale of the graph
+            var tick_scale = d3.scale.linear()
+              .domain([0, 1])
+              .range([chart.min_bar_size, chart.height_scale(chart.data_value_accessor(d))]);
+
             return function(t) {
-              this.textContent = i(t);
+              this.textContent = Math.round(chart.height_scale.invert(tick_scale(t)));
             };
           })
           .delay(this.animation_delay)
@@ -226,7 +309,7 @@
       //otherwise, pass them through the prettify_number routine
       else {
         labels_inside.selectAll('div')
-          .data(this.data, this.data_id_key_accessor)
+          .data(this.data, this.data_id_accessor)
           .html(function(d, i) {
             return chart.prettify_number(chart.data_value_accessor(d));
           });
@@ -235,35 +318,42 @@
       this.has_rendered = true;
     };
   
-    //TODO ability to parameterize based on range of dataset?
+    Chart.prototype.LN10x2 = Math.LN10 * 2;
+    //TODO ability to parameterize based on domain of dataset?
     Chart.prototype.prettify_number = function(num) {
       var suffixes = ' kMBT';
       var abs = Math.abs(num);
       var mag;
-      if (abs < 1)
+      if (abs < 1) {
         mag = Math.floor(Math.log(abs) / Math.LN10);
-      else
+      }
+      else {
         //average with magnitude of num + 1 to correct for floating-point error
-        mag = Math.floor((Math.log(abs) / Math.LN10 + Math.log(abs + 1) / Math.LN10) / 2);
+        mag = Math.floor((Math.log(abs) + Math.log(abs + 1)) / (Chart.prototype.LN10x2));
+      }
   
       if (mag >= 3) {
         var index = Math.floor(mag / 3);
         var suffix;
-        if (suffixes.length > index)
+        if (suffixes.length > index) {
           suffix = suffixes.charAt(index);
-        else
+        }
+        else {
           suffix = '*10<sup>' + mag + '</sup>';
+        }
         return (Math.round(num / Math.pow(10, mag - (mag % 3) - 1)) / 10.0) + suffix;
       }
-      else if (mag <= -3)
+      else if (mag <= -3) {
         return Math.round(num / Math.pow(10, mag)) + 'e' + mag;
-      else
+      }
+      else {
         return num.toString();
+      }
     };
   
     return Chart;
   };
-  if (typeof define == 'function' && define.amd) {
+  if (typeof define === 'function' && define.amd) {
     define('barchart', ['d3'], defineBarChart);
   }
   else {
