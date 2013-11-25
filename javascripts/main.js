5c990ef016dac48b1f1505d918a277ab24f271f8
Ben Robbins <ben.robbins@gmail.com>
2013-11-24 18:41:59 -0600

    update for version 0.3.1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,29 @@
 requirejs.config({
   shim: {
-    'd3': {
-      exports: 'd3'
-    }
+    'd3': {exports: 'd3'}
   }
 });
-define([
-  'barchart'
-],
-function(BarChart) {
+define(['barchart'], function(BarChart) {
   var bc = new BarChart({
-    data: [
-      {"id": 0, "name": "item1", "value": 12},
-      {"id": 1, "name": "item2", "value": 20},
-      {"id": 2, "name": "item3", "value": 30},
-      {"id": 3, "name": "item4", "value": 70},
-      {"id": 4, "name": "item5", "value": 63},
-      {"id": 5, "name": "item6", "value": 35}
-    ],
+    data: [[
+      {"name": "#1", "value": 12},
+      {"name": "#2", "value": 20},
+      {"name": "#3", "value": 30},
+      {"name": "#4", "value": 70},
+      {"name": "#5", "value": 63},
+      {"name": "#6", "value": 35}
+    ],[
+      {"name": "#1'", "value": 2},
+      {"name": "#2'", "value": 10},
+      {"name": "#3'", "value": 3},
+      {"name": "#4'", "value": 7},
+      {"name": "#5'", "value": 6},
+      {"name": "#6'", "value": 3}
+    ]],
+    bar_colors: ['#00AB8E', '#33CCDD'],
+    label_inside_colors: ['#FFF', '#333'],
     auto_scale: true,
     container: document.getElementById('chart-container')
   });
   bc.render();
-  console.log(document.getElementById('chart-container'));
 });
